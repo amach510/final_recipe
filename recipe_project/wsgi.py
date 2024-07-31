a5ed6be22f8f4cac2f349ead7f88f7b2549fcb57
Anthony <amach510@gmail.com>
2024-07-31 03:28:29 -0700

    Update wsgi.py

--- recipe_project/wsgi.py
+++ recipe_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "A2_Recipe_App.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "final_recipe.settings")
 
 application = get_wsgi_application()
