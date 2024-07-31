ab7d6e5fc710c3b1989bd6906f8388a85a9f3432
Anthony <amach510@gmail.com>
2024-07-31 04:46:46 -0700

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from users.models import CustomUser
 
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'pic', 'bio')
+    search_fields = ('user__username', 'name')
+
 # Register your models here.
 admin.site.register(CustomUser)
