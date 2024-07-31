632aca6450a9b6d4e662d59b2529f3e7e08dfb60
Anthony <amach510@gmail.com>
2024-07-31 04:13:39 -0700

    revert changes from final_recipe to recipe_project

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn final_recipe.wsgi --log-file -
+web: gunicorn recipe_project.wsgi --log-file -
--- recipe_project/wsgi.py
+++ recipe_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for final_recipe project.
+WSGI config for recipe_project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "final_recipe.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recipe_project.settings")
 
 application = get_wsgi_application()
